fcec44c9b03b8189c8e421fa8dfc4e1beace0e6d
marlonnn <zhongw_1988@126.com>
2017-01-17 13:59:50 +0800

    添加未提交的两个文件SplitFrameForm.cs、SplitFrameForm.Designer.cs

---
+++ SplitFrame/SplitFrame/SplitFrameForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SplitFrame
+{
+    partial class SplitFrameForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplitFrameForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtSelectDirectory = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtSelectDirectory);
+            this.panel1.Controls.Add(this.btnSelect);
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(497, 45);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtSelectDirectory
+            // 
+            this.txtSelectDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSelectDirectory.Location = new System.Drawing.Point(83, 12);
+            this.txtSelectDirectory.Name = "txtSelectDirectory";
+            this.txtSelectDirectory.ReadOnly = true;
+            this.txtSelectDirectory.Size = new System.Drawing.Size(403, 20);
+            this.txtSelectDirectory.TabIndex = 1;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(3, 9);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(74, 23);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "打开";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(2, 63);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(496, 10);
+            this.progressBar.TabIndex = 5;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabel1.Location = new System.Drawing.Point(1, 53);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(497, 29);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkClicked);
+            // 
+            // SplitFrameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 83);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.linkLabel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SplitFrameForm";
+            this.Text = "拆帧工具";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtSelectDirectory;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ SplitFrame/SplitFrame/SplitFrameForm.cs
@@ -0,0 +1,153 @@
+﻿using DirectShowLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplitFrame
+{
+    public partial class SplitFrameForm : Form
+    {
+        private string _videoFileName;
+
+        private bool _canStep = false;
+        /// <summary>
+        /// 处理视频相关DirectShowLib接口
+        /// </summary>
+        private IGraphBuilder graphBuilder = null;
+        private IMediaControl mediaCtrl = null;
+        private IMediaEventEx mediaEvt = null;
+        private IMediaPosition mediaPos = null;
+        private IVideoFrameStep frameStep = null;
+        private IVideoWindow videoWin = null;
+
+        private const int WM_GRAPHNOTIFY = 0x00008001;
+        private const int WS_CHILD = 0x40000000;
+        private const int WS_CLIPCHILDREN = 0x02000000;
+        private const int WS_CLIPSIBLINGS = 0x04000000;
+        private const int WM_MOVE = 0x00000003;
+        private const int EC_COMPLETE = 0x00000001;
+
+        private GrabFrames _grabFrames;
+        public delegate void ReportProgress(double progress);
+
+        public SplitFrameForm()
+        {
+            InitializeComponent();
+            this.progressBar.Visible = false;
+        }
+
+        // 
+        //创建filter graph
+        //
+        private void InitInterfaces()
+        {
+            try
+            {
+                graphBuilder = (IGraphBuilder)new FilterGraph();
+                mediaCtrl = (IMediaControl)graphBuilder;
+                mediaEvt = (IMediaEventEx)graphBuilder;
+                mediaPos = (IMediaPosition)graphBuilder;
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("Couldn't start");
+            }
+        }
+
+        //
+        //This method stop the filter graph and ensures that we stop
+        //sending messages to our window
+        //
+        private void CloseInterfaces()
+        {
+            if (mediaCtrl != null)
+            {
+                mediaCtrl.StopWhenReady();
+                mediaEvt.SetNotifyWindow((IntPtr)0, WM_GRAPHNOTIFY, (IntPtr)0);
+            }
+            mediaCtrl = null;
+            mediaEvt = null;
+            mediaPos = null;
+            if (_canStep)
+                frameStep = null;
+            videoWin = null;
+            if (graphBuilder != null)
+                Marshal.ReleaseComObject(this.graphBuilder);
+            graphBuilder = null;
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = @"All Files (*.*) | *.*|AVI Files (*.avi)|*.avi|MOV Files (*.mov)|*.mov|MP4 Files (*.mp4)|*.mp4|FLV Files (*.flv)|*.flv";
+            openFileDialog.InitialDirectory = System.Environment.CurrentDirectory;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                CloseInterfaces();
+                _videoFileName = openFileDialog.FileName;
+                this.txtSelectDirectory.Text = _videoFileName;
+                InitInterfaces();
+                var folder = System.IO.Path.GetFileNameWithoutExtension(openFileDialog.FileName);
+                SplitFrame(_videoFileName, folder);
+            }
+        }
+
+        private void SplitFrame(string fileName, string folder)
+        {
+            _grabFrames = new GrabFrames(fileName);
+            _grabFrames.StoragePath = folder;
+            _grabFrames.ReportProgressHandler += ReportProgressHandler;
+            this.progressBar.Visible = true;
+            SetResult(false);
+            this.btnSelect.Enabled = false;
+        }
+
+        private void ReportProgressHandler(double progress)
+        {
+            if (this.progressBar.InvokeRequired)
+            {
+                ReportProgress d = new ReportProgress(ReportProgressHandler);
+                this.progressBar.Invoke(d, new object[] { progress });
+            }
+            else
+            {
+                int value = (int)((progress + 1) * 100 / (this._grabFrames.MediaInfo.Duration * this._grabFrames.MediaInfo.FPS));
+                if (value >= 100)
+                {
+                    this.progressBar.Value = 0;
+                    this.progressBar.Visible = false;
+                    SetResult(true);
+                    this.btnSelect.Enabled = true;
+                }
+                this.progressBar.Value = value;
+            }
+        }
+
+        private void SetResult(bool isEnable)
+        {
+            this.linkLabel1.Visible = isEnable;
+            if (isEnable)
+                this.linkLabel1.Text = string.Format("{0}\\{1}", System.Environment.CurrentDirectory, _grabFrames.StoragePath);
+            else
+                this.linkLabel1.Text = "";
+        }
+
+        private void LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("Explorer.exe", _grabFrames.Folder);
+            }
+            catch (Exception ee)
+            {
+            }
+        }
+    }
+}
